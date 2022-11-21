09be6b679b7d38e62bc2b488536462c80532a4a7
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-21 16:56:07 +0300

    İlk Yükleme

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def index2(request):
+    return render(request, 'home/index2.html')
+
+
+def index3(request):
+    return render(request, 'home/index3.html')
