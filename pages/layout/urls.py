8f6b1af43d5febabc62c6ca4ad5e1bf81380c240
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-23 15:42:02 +0300

    Pages Layout Bölümü Düzenlendi

--- pages/layout/urls.py
+++ pages/layout/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('top-nav/', views.top_nav, name='top-nav')
+    path('top-nav/', views.top_nav, name='top-nav'),
+    path('top-nav-sidebar/', views.top_nav_sidebar, name='top-nav-sidebar'),
+    path('boxed/', views.boxed, name='boxed'),
+    path('fixed-sidebar/', views.fixed_sidebar, name='fixed-sidebar'),
+    path('fixed-sidebar-custom/', views.fixed_sidebar_custom, name='fixed-sidebar-custom'),
+    path('fixed-topnav/', views.fixed_topnav, name='fixed-topnav'),
+    path('fixed-footer/', views.fixed_footer, name='fixed-footer'),
+    path('collapsed-sidebar', views.collapsed_sidebar, name='collapsed-sidebar'),
+    
+
+
+
 ]
--- pages/layout/views.py
+++ pages/layout/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def top_nav(request):
     return render(request,'pages/layout/top-nav.html')
+
+def top_nav_sidebar(request):
+    return render(request,'pages/layout/top-nav-sidebar.html')
+
+def boxed(request):
+    return render(request, 'pages/layout/boxed.html')
+
+def fixed_sidebar(request):
+    return render(request, 'pages/layout/fixed-sidebar.html')
+
+def fixed_sidebar_custom(request):
+    return render(request, 'pages/layout/fixed-sidebar-custom.html')
+
+def fixed_topnav(request):
+    return render(request, 'pages/layout/fixed-topnav.html')
+
+def fixed_footer(request):
+    return render(request, 'pages/layout/fixed-footer.html')
+
+def collapsed_sidebar(request):
+    return render(request, 'pages/layout/collapsed-sidebar.html')
