5aad1c2f23b616861e02ede0b4c81c943b64300a
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-28 14:08:30 +0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.root, name='root'),
+    path('index/', views.index, name='index'),
     path('index2/', views.index2, name='index2'),
     path('index3/', views.index3, name='index3'),
     path('iframe/', views.iframe, name='iframe'),
