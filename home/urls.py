1f31db5a5c13c6f749d002d4b1e9473b6e987048
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-28 12:48:47 +0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('index2/', views.index2, name='index2'),
     path('index3/', views.index3, name='index3'),
     path('iframe/', views.iframe, name='iframe'),
+    path('iframe-dark/', views.iframe_dark, name='iframe-dark'),
 
 ]
